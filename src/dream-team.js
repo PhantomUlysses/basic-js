8c0aa3e918fbba8c747b5df790cce51e2e632fde
Nikita Stasiulevich <nikitastas1694@gmail.com>
2021-03-28 22:12:25 +0300

    feat: add count cats solve

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!members || !members.length) return false;
+  
+  return members.reduce(function (p, i) {
+    return p = typeof i === 'string'
+    ? p.concat(i.trim()[0].toUpperCase())
+    : p
+  }, []).sort().join('')
+}
