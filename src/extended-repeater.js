2276fad130b92cc5105a583871d75b78325f884e
Nikita Stasiulevich <nikitastas1694@gmail.com>
2021-03-29 23:07:01 +0300

    feat: add solve

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let repeatTimes = options.repeatTimes || 1;
+  let separator = options.separator || '+';
+  let addition = options.addition || '';
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let additionSeparator = options.additionSeparator || '|';
+
+  if (typeof options.addition !== 'string' && options.addition ==! undefined) {
+    addition = String(addition);
+  } else if (options.addition === false) {
+    addition = "false";
+  } else if (options.addition === null) {
+    addition = "null";
+  }
+
+  if (typeof str !== 'string') {
+    str = String(str);
+  }
+
+  let array = [];
+  let repeat = [];
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    array.push(addition);
+  }
+
+  let repStr = str + array.join(additionSeparator);
+  
+  for (let i = 0; i < repeatTimes; i++) {
+    repeat.push(repStr);
+  }
+
+  let result = repeat.join(separator);
+  return result;
+
+
 };
   
